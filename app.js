f7ef641bad9664e1a4e201e3889e2e1b7126e9a5
Matthew Simmons <matthewsimmonsdev@gmail.com>
2021-06-01 10:59:30 -0400

    Add player placing functionality

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const gameSpaces = document.querySelectorAll('.gamespace');
+let turnOrder = 1;
+//gameboard
 const gameBoard = (() => {
   const gameBoardArr = [
     [1,2,3],
@@ -7,3 +10,26 @@ const gameBoard = (() => {
   return gameBoardArr;
 })();
 
+//Players Factory Function
+const player = (turnOrder, piece) => {
+  return {turnOrder, piece};
+};
+
+const playerOne = player(1, 'X');
+const playerTwo = player(2, 'O');
+
+//add pieces to board
+function placePiece(e){
+  if (turnOrder === playerOne.turnOrder){
+    turnOrder++;
+    e.target.textContent = playerOne.piece;
+  }
+  else if (turnOrder === playerTwo.turnOrder){
+    turnOrder--;
+    e.target.textContent = playerTwo.piece;
+  }
+}
+
+gameSpaces.forEach(element => {
+  element.addEventListener('click', placePiece)
+});
