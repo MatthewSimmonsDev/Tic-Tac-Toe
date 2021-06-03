fb9f34bfb736dadbaad5bf94b7db4c2a738f5a18
Matthew Simmons <matthewsimmonsdev@gmail.com>
2021-06-02 22:03:20 -0400

    Started over from the beginning.  Didn't like what I had

--- app.js
+++ app.js
@@ -1,35 +1,77 @@
 const gameSpaces = document.querySelectorAll('.gamespace');
-let turnOrder = 1;
-//gameboard
-const gameBoard = (() => {
-  const gameBoardArr = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-  ];
-  return gameBoardArr;
-})();
+const results = document.querySelector('.results');
+const resetBtn = document.querySelector('.reset-btn');
+const gameBoard = ['','','','','','','','','']
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,7],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
-//Players Factory Function
-const player = (turnOrder, piece) => {
-  return {turnOrder, piece};
-};
 
-const playerOne = player(1, 'X');
-const playerTwo = player(2, 'O');
+let round = 1;
 
-//add pieces to board
-function placePiece(e){
-  if (turnOrder === playerOne.turnOrder){
-    turnOrder++;
-    e.target.textContent = playerOne.piece;
-  }
-  else if (turnOrder === playerTwo.turnOrder){
-    turnOrder--;
-    e.target.textContent = playerTwo.piece;
+const player = (piece) => {
+  return {piece};
+}
+
+const playerOne = player('X');
+const playerTwo = player('O');
+
+function playerPiece(index, player){
+  gameSpaces[index].textContent = player.piece;
+  gameBoard[index] = player.piece;
+  round++;
+  // checkWin(playerOne);
+  // checkWin(playerTwo);
+}
+
+//if gameboard content matches any of the winning combos  return victory
+// compare indexes from winning combos with the game board array
+function checkWin(player){
+  winningCombos.forEach((item) => {
+    if (gameBoard[item[0]] === player.piece && gameBoard[item[1]] === player.piece && gameBoard[item[2]] === player.piece ){
+      results.textContent = `${player.piece} is the WINNER!`
+    }
+  })
+  checkDraw();
+
+}
+
+function checkDraw(){
+  if (round === 10){
+    results.textContent = 'DRAW!'
   }
 }
 
-gameSpaces.forEach(element => {
-  element.addEventListener('click', placePiece)
-});
+results.textContent = `${playerOne.piece} goes first`
+
+for(let i = 0; i < gameSpaces.length; i++){
+  gameSpaces[i].addEventListener('click', () => {
+    if(round % 2 !== 0){
+      playerPiece(i, playerOne);
+      results.textContent = `${playerTwo.piece}'s turn`
+      checkWin(playerOne);
+      checkWin(playerTwo);
+    } else {
+      playerPiece(i, playerTwo);
+      results.textContent = `${playerOne.piece}'s turn`
+      checkWin(playerOne);
+      checkWin(playerTwo);
+    }
+  })
+}
+
+resetBtn.addEventListener('click', () => {
+  round = 1;
+  for (let i = 0; i < gameBoard.length; i++){
+    gameBoard[i] = '';
+    gameSpaces[i].textContent = ''; 
+    results.textContent = 'X goes first';
+  }
+})
