1d9a229b507c831fd45f0ccd23f5bfc439bb415b
Matthew Simmons <matthewsimmonsdev@gmail.com>
2021-06-03 10:08:50 -0400

    Refactored code to use factory and module patterns

--- app.js
+++ app.js
@@ -1,68 +1,66 @@
-const gameSpaces = document.querySelectorAll('.gamespace');
-const results = document.querySelector('.results');
-const resetBtn = document.querySelector('.reset-btn');
-const gameBoard = ['','','','','','','','','']
-const winningCombos = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,7],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
 
-
-let round = 1;
 
 const player = (piece) => {
   return {piece};
 }
 
-const playerOne = player('X');
-const playerTwo = player('O');
 
-function playerPiece(index, player){
-  gameSpaces[index].textContent = player.piece;
-  gameBoard[index] = player.piece;
-  round++;
-  // checkWin(playerOne);
-  // checkWin(playerTwo);
-}
+const gameController = (() => {
+  let round = 1;
+  const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,7],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
 
-//if gameboard content matches any of the winning combos  return victory
-// compare indexes from winning combos with the game board array
-function checkWin(player){
-  winningCombos.forEach((item) => {
-    if (gameBoard[item[0]] === player.piece && gameBoard[item[1]] === player.piece && gameBoard[item[2]] === player.piece ){
-      results.textContent = `${player.piece} is the WINNER!`
-    }
-  })
-  checkDraw();
+  const playerOne = player('X');
+  const playerTwo = player('O');
 
-}
+  const gameSpaces = document.querySelectorAll('.gamespace');
+  const results = document.querySelector('.results');
+  const resetBtn = document.querySelector('.reset-btn');
+  const gameBoard = ['','','','','','','','','']
 
-function checkDraw(){
-  if (round === 10){
-    results.textContent = 'DRAW!'
+  function checkWin(player){
+    winningCombos.forEach((item) => {
+      if (gameBoard[item[0]] === player.piece && gameBoard[item[1]] === player.piece && gameBoard[item[2]] === player.piece ){
+        results.textContent = `${player.piece} is the WINNER!`
+      }
+    })
+    checkDraw();
   }
-}
+    function playerPiece(index, player){
+      gameSpaces[index].textContent = player.piece;
+      gameBoard[index] = player.piece;
+      round++;
+    }
+  
 
-results.textContent = `${playerOne.piece} goes first`
+  function checkDraw(){
+    if (round === 10){
+      results.textContent = 'DRAW!'
+    }
+  }
+
+  results.textContent = `${playerOne.piece} goes first`
 
 for(let i = 0; i < gameSpaces.length; i++){
   gameSpaces[i].addEventListener('click', () => {
     if(round % 2 !== 0){
-      playerPiece(i, playerOne);
+      gameController.playerPiece(i, playerOne);
       results.textContent = `${playerTwo.piece}'s turn`
-      checkWin(playerOne);
-      checkWin(playerTwo);
+      gameController.checkWin(playerOne);
+      gameController.checkWin(playerTwo);
     } else {
-      playerPiece(i, playerTwo);
+      gameController.playerPiece(i, playerTwo);
       results.textContent = `${playerOne.piece}'s turn`
-      checkWin(playerOne);
-      checkWin(playerTwo);
+      gameController.checkWin(playerOne);
+      gameController.checkWin(playerTwo);
     }
   })
 }
@@ -75,3 +73,5 @@ resetBtn.addEventListener('click', () => {
     results.textContent = 'X goes first';
   }
 })
+  return {winningCombos, checkWin, playerPiece};
+})();
